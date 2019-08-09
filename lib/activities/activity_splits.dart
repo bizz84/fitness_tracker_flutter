0fceb10f3d03e5f59646aadf9baea76949acb3f2
bizz84 <bizz84@gmail.com>
2019-08-09 12:22:24 +0100

    Move helpers to bottom

--- lib/activities/activity_splits.dart
+++ lib/activities/activity_splits.dart
@@ -9,21 +9,10 @@ class ActivitySplits extends StatelessWidget {
   const ActivitySplits(this.splits);
   final List<Split> splits;
 
-  int maxPace() {
-    final paces = splits.map((split) => split.pace.inSeconds);
-    return paces.reduce((value, element) => max(value, element));
-  }
-
-  int minPace() {
-    final paces = splits.map((split) => split.pace.inSeconds);
-    return paces.reduce((value, element) => min(value, element));
-  }
-
   @override
   Widget build(BuildContext context) {
     final max = maxPace();
     final min = minPace();
-    //print('max: ${maxPace()}, min: ${minPace()}');
     return Column(
       crossAxisAlignment: CrossAxisAlignment.stretch,
       children: <Widget>[
@@ -39,8 +28,18 @@ class ActivitySplits extends StatelessWidget {
             split: split,
             maxPace: max,
             minPace: min,
-          )
+          ),
       ],
     );
+  }
+
+  int maxPace() {
+    final paces = splits.map((split) => split.pace.inSeconds);
+    return paces.reduce((value, element) => max(value, element));
+  }
+
+  int minPace() {
+    final paces = splits.map((split) => split.pace.inSeconds);
+    return paces.reduce((value, element) => min(value, element));
   }
 }
